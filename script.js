2cf76172c48de677cc4377b61279d9ba9be6bdf6
Anne <anne.dupin91@gmail.com>
2021-05-01 16:31:54 +0200

    Deuxième envoie

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ else {
     $('nav').removeClass('scroll');
 }
 });
+
+
+$(".bar").each(function(){
+    $(this).find(".bar-inner").animate({
+      width: $(this).attr("data-width")
+    },2000)
+  });

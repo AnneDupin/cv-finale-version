25b6f2fb9dda75cf3be2fe18ebdd923a05e60143
Anne <anne.dupin91@gmail.com>
2021-05-02 16:05:29 +0200

    Quatrième

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ $(".bar").each(function(){
 
 
   // Portfolio
+jQuery('#portfolio').mixItUp({  
+
+  selectors: {
+    target: '.tile',
+    filter: '.filter',
+    sort: '.sort-btn'
+  },
+  
+      animation: {
+      animateResizeContainer: false,
+      effects: 'fade scale'
+    }
+
+});
